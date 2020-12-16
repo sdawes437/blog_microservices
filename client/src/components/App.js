fab5e1b2a07c01fd27ae650047c3fc701a887149
sdawes437 <sdawes437@gmail.com>
2020-12-16 20:03:32 +0000

    front end completed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import PostCreate from "./PostCreate";
+import PostList from "./PostList";
+import "./App.css";
 
 export default () => {
   return (
     <div className="container">
       <h1>Create Post</h1>
       <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
 };

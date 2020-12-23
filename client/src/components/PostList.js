f3e905b387fb61d1571040a74690268af4c29753
sdawes437 <sdawes437@gmail.com>
2020-12-23 16:52:08 +0000

    added query service for returning comments

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -8,8 +8,8 @@ export default () => {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get("http://localhost:4000/posts");
-
+      const res = await axios.get("http://localhost:4002/posts");
+      console.log(res.data);
       setPosts(res.data);
     };
 
@@ -25,7 +25,7 @@ export default () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
+          <CommentList comments={post.comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
